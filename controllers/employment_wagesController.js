4001039cf40f51b019700905bfab5419b49edfc3
Evelyn Mejia <enmejia@espol.edu.ec>
2021-11-16 00:29:40 -0500

    method update updated

--- controllers/employment_wagesController.js
+++ controllers/employment_wagesController.js
@@ -9,7 +9,8 @@ var options = {
 		'fields.avg_wkly_wage',
 		'fields.month1_emplvl',
 		'fields.period',
-	]	
+	],	
+	sort: {  _id: -1 }
 };
 
 /**
@@ -27,7 +28,7 @@ module.exports = {
 			if (req.query.page) options.page = req.query.page;
 			if (req.query.limit) options.limit = req.query.limit;
 		}
-		Employment_wagesModel.paginate({}, options)
+		Employment_wagesModel.paginate({}, options)		
 		.then(data => {
 			res.send({
 				currentPage: data.page,
@@ -114,56 +115,14 @@ module.exports = {
                 });
             }
 
-            employment_wages.industry_code = req.body.industry_code ? req.body.industry_code : employment_wages.industry_code;
-			employment_wages.size_code = req.body.size_code ? req.body.size_code : employment_wages.size_code;
-			employment_wages.lq_qtrly_contributions = req.body.lq_qtrly_contributions ? req.body.lq_qtrly_contributions : employment_wages.lq_qtrly_contributions;
-			employment_wages.month2_emplvl = req.body.month2_emplvl ? req.body.month2_emplvl : employment_wages.month2_emplvl;
-			employment_wages.year = req.body.year ? req.body.year : employment_wages.year;
-			employment_wages.lq_qtrly_estabs_count = req.body.lq_qtrly_estabs_count ? req.body.lq_qtrly_estabs_count : employment_wages.lq_qtrly_estabs_count;
-			employment_wages.month3_emplvl = req.body.month3_emplvl ? req.body.month3_emplvl : employment_wages.month3_emplvl;
-			employment_wages.lq_month3_emplvl = req.body.lq_month3_emplvl ? req.body.lq_month3_emplvl : employment_wages.lq_month3_emplvl;
-			employment_wages.lq_avg_wkly_wage = req.body.lq_avg_wkly_wage ? req.body.lq_avg_wkly_wage : employment_wages.lq_avg_wkly_wage;
-			employment_wages.total_qtrly_wages = req.body.total_qtrly_wages ? req.body.total_qtrly_wages : employment_wages.total_qtrly_wages;
-			employment_wages.industry_title = req.body.industry_title ? req.body.industry_title : employment_wages.industry_title;
-			employment_wages.oty_total_qtrly_wages_pct_chg = req.body.oty_total_qtrly_wages_pct_chg ? req.body.oty_total_qtrly_wages_pct_chg : employment_wages.oty_total_qtrly_wages_pct_chg;
-			employment_wages.oty_qtrly_contributions_pct_chg = req.body.oty_qtrly_contributions_pct_chg ? req.body.oty_qtrly_contributions_pct_chg : employment_wages.oty_qtrly_contributions_pct_chg;
-			employment_wages.oty_qtrly_estabs_count_pct_chg = req.body.oty_qtrly_estabs_count_pct_chg ? req.body.oty_qtrly_estabs_count_pct_chg : employment_wages.oty_qtrly_estabs_count_pct_chg;
-			employment_wages.oty_month2_emplvl_pct_chg = req.body.oty_month2_emplvl_pct_chg ? req.body.oty_month2_emplvl_pct_chg : employment_wages.oty_month2_emplvl_pct_chg;
-			employment_wages.agglvl_code = req.body.agglvl_code ? req.body.agglvl_code : employment_wages.agglvl_code;
-			employment_wages.oty_qtrly_estabs_count_chg = req.body.oty_qtrly_estabs_count_chg ? req.body.oty_qtrly_estabs_count_chg : employment_wages.oty_qtrly_estabs_count_chg;
-			employment_wages.lq_total_qtrly_wages = req.body.lq_total_qtrly_wages ? req.body.lq_total_qtrly_wages : employment_wages.lq_total_qtrly_wages;
-			employment_wages.size_title = req.body.size_title ? req.body.size_title : employment_wages.size_title;
-			employment_wages.oty_avg_wkly_wage_chg = req.body.oty_avg_wkly_wage_chg ? req.body.oty_avg_wkly_wage_chg : employment_wages.oty_avg_wkly_wage_chg;
-			employment_wages.oty_month1_emplvl_chg = req.body.oty_month1_emplvl_chg ? req.body.oty_month1_emplvl_chg : employment_wages.oty_month1_emplvl_chg;
-			employment_wages.oty_taxable_qtrly_wages_pct_chg = req.body.oty_taxable_qtrly_wages_pct_chg ? req.body.oty_taxable_qtrly_wages_pct_chg : employment_wages.oty_taxable_qtrly_wages_pct_chg;
-			employment_wages.area_titl = req.body.area_titl ? req.body.area_titl : employment_wages.area_titl;
-			employment_wages.own_code = req.body.own_code ? req.body.own_code : employment_wages.own_code;
-			employment_wages.oty_month3_emplvl_chg = req.body.oty_month3_emplvl_chg ? req.body.oty_month3_emplvl_chg : employment_wages.oty_month3_emplvl_chg;
-			employment_wages.qtrly_estabs_count = req.body.qtrly_estabs_count ? req.body.qtrly_estabs_count : employment_wages.qtrly_estabs_count;
-			employment_wages.oty_avg_wkly_wage_pct_chg = req.body.oty_avg_wkly_wage_pct_chg ? req.body.oty_avg_wkly_wage_pct_chg : employment_wages.oty_avg_wkly_wage_pct_chg;
-			employment_wages.oty_month1_emplvl_pct_chg = req.body.oty_month1_emplvl_pct_chg ? req.body.oty_month1_emplvl_pct_chg : employment_wages.oty_month1_emplvl_pct_chg;
-			employment_wages.oty_qtrly_contributions_chg = req.body.oty_qtrly_contributions_chg ? req.body.oty_qtrly_contributions_chg : employment_wages.oty_qtrly_contributions_chg;
-			employment_wages.oty_month3_emplvl_pct_chg = req.body.oty_month3_emplvl_pct_chg ? req.body.oty_month3_emplvl_pct_chg : employment_wages.oty_month3_emplvl_pct_chg;
-			employment_wages.period = req.body.period ? req.body.period : employment_wages.period;
-			employment_wages.own_title = req.body.own_title ? req.body.own_title : employment_wages.own_title;
-			employment_wages.oty_month2_emplvl_chg = req.body.oty_month2_emplvl_chg ? req.body.oty_month2_emplvl_chg : employment_wages.oty_month2_emplvl_chg;
-			employment_wages.lq_month2_emplvl = req.body.lq_month2_emplvl ? req.body.lq_month2_emplvl : employment_wages.lq_month2_emplvl;
-			employment_wages.oty_taxable_qtrly_wages_chg = req.body.oty_taxable_qtrly_wages_chg ? req.body.oty_taxable_qtrly_wages_chg : employment_wages.oty_taxable_qtrly_wages_chg;
-			employment_wages.avg_wkly_wage = req.body.avg_wkly_wage ? req.body.avg_wkly_wage : employment_wages.avg_wkly_wage;
-			employment_wages.qtr = req.body.qtr ? req.body.qtr : employment_wages.qtr;
-			employment_wages.area_fips = req.body.area_fips ? req.body.area_fips : employment_wages.area_fips;
-			employment_wages.qtrly_contributions = req.body.qtrly_contributions ? req.body.qtrly_contributions : employment_wages.qtrly_contributions;
-			employment_wages.month1_emplvl = req.body.month1_emplvl ? req.body.month1_emplvl : employment_wages.month1_emplvl;
-			employment_wages.agglvl_title = req.body.agglvl_title ? req.body.agglvl_title : employment_wages.agglvl_title;
-			employment_wages.lq_disclosure_code = req.body.lq_disclosure_code ? req.body.lq_disclosure_code : employment_wages.lq_disclosure_code;
-			employment_wages.lq_taxable_qtrly_wages = req.body.lq_taxable_qtrly_wages ? req.body.lq_taxable_qtrly_wages : employment_wages.lq_taxable_qtrly_wages;
-			employment_wages.disclosure_code = req.body.disclosure_code ? req.body.disclosure_code : employment_wages.disclosure_code;
-			employment_wages.taxable_qtrly_wages = req.body.taxable_qtrly_wages ? req.body.taxable_qtrly_wages : employment_wages.taxable_qtrly_wages;
-			employment_wages.lq_month1_emplvl = req.body.lq_month1_emplvl ? req.body.lq_month1_emplvl : employment_wages.lq_month1_emplvl;
-			employment_wages.oty_disclosure_code = req.body.oty_disclosure_code ? req.body.oty_disclosure_code : employment_wages.oty_disclosure_code;
-			employment_wages.oty_total_qtrly_wages_chg = req.body.oty_total_qtrly_wages_chg ? req.body.oty_total_qtrly_wages_chg : employment_wages.oty_total_qtrly_wages_chg;
-			
-            employment_wages.save(function (err, employment_wages) {
+			employment_wages.fields.area_title = req.body.fields.area_title ? req.body.fields.area_title : employment_wages.fields.area_title,			
+			employment_wages.fields.industry_code = req.body.fields.industry_code ? req.body.fields.industry_code : employment_wages.fields.industry_code,
+			employment_wages.fields.industry_title = req.body.fields.industry_title ? req.body.fields.industry_title : employment_wages.fields.industry_title,
+			employment_wages.fields.total_qtrly_wages = req.body.fields.total_qtrly_wages ? req.body.fields.total_qtrly_wages : employment_wages.fields.total_qtrly_wages,
+			employment_wages.fields.avg_wkly_wage = req.body.fields.avg_wkly_wage ? req.body.fields.avg_wkly_wage : employment_wages.fields.avg_wkly_wage,
+			employment_wages.fields.period = req.body.fields.period ? req.body.fields.period : employment_wages.fields.period,
+			employment_wages.fields.month1_emplvl = req.body.fields.month1_emplvl ? req.body.fields.month1_emplvl : employment_wages.fields.month1_emplvl,
+        	employment_wages.save(function (err, employment_wages) {
                 if (err) {
                     return res.status(500).json({
                         message: 'Error when updating employment_wages.',
