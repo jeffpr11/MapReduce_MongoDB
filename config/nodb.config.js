143b1769d9cd33ce051f02149332b53f2f90cdd0
Jeffrey Prado <Jefgapra@espol.edu.ec>
2021-11-16 00:06:04 -0500

    MongoDB conexion clean

---
+++ config/nodb.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    HOST: "cluster0.cg4bc.mongodb.net",
+    USER: "bddavanzadas",
+    PASSWORD: "BDDAvanzadas2021",
+    noDB: "BDDA",
+    dialect: "mongodb+srv"
+};
--- models/index.js
+++ models/index.js
@@ -1,15 +1,13 @@
-// const nodbConfig = require("../config/nodb.config.js");
+const nodbConfig = require("..\\config\\nodb.config.js");
 
 const mongoose = require('mongoose');
 
-// nodbConfig.dialect + '://' + nodbConfig.HOST + '/' + nodbConfig.noDB;
-//const db_path ='mongodb://proyecto1bda:SZnlfUeKjdnpyl0ovU6S8kAaB4gjiyVA312m7SGAmUmniyiiyGEDwnEiFAA1OwAfTUOIUa4sAwcNHIieNGnYHw==@proyecto1bda.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@proyecto1bda@'
-const db_path ='mongodb+srv://bddavanzadas:BDDAvanzadas2021@cluster0.cg4bc.mongodb.net/BDDA?retryWrites=true&w=majority'
+const db_path = nodbConfig.dialect + '://' + nodbConfig.USER + ':' + nodbConfig.PASSWORD + '@' + nodbConfig.HOST + '/' + nodbConfig.noDB + '?retryWrites=true&w=majority';
 
 mongoose.connect(db_path)
-    .then((db) =>{
-        console.log('DB connnection successful!', db);
-    } )
+    .then((db) => {
+        console.log('DB connnection successful!');
+    })
     .catch(err => {
         console.error.bind(console, 'MongoDB connection error:')
     });
