540e0f4784a1fc6f8d3c37e44c8040b6597292df
Jeffrey Prado <Jefgapra@espol.edu.ec>
2021-11-22 01:40:49 -0500

    azure config

--- config/nodb.config.js
+++ config/nodb.config.js
@@ -3,5 +3,12 @@ module.exports = {
     USER: "bddavanzadas",
     PASSWORD: "BDDAvanzadas2021",
     noDB: "BDDA",
-    dialect: "mongodb+srv"
+    dialect: "mongodb+srv",
+    end: "?retryWrites=true&w=majority",
+    dialectAZ: "mongodb",
+    USERAZ: "proyecto1bda",
+    PASSWORDAZ: "SZnlfUeKjdnpyl0ovU6S8kAaB4gjiyVA312m7SGAmUmniyiiyGEDwnEiFAA1OwAfTUOIUa4sAwcNHIieNGnYHw==",
+    HOSTAZ: "proyecto1bda.mongo.cosmos.azure.com:10255",
+    noDBAZ: "test",
+    endAZ: "?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@proyecto1bda@"
 };
--- models/index.js
+++ models/index.js
@@ -2,9 +2,10 @@ const nodbConfig = require("..\\config\\nodb.config.js");
 
 const mongoose = require('mongoose');
 
-const db_path = nodbConfig.dialect + '://' + nodbConfig.USER + ':' + nodbConfig.PASSWORD + '@' + nodbConfig.HOST + '/' + nodbConfig.noDB + '?retryWrites=true&w=majority';
+const db_path = nodbConfig.dialect + '://' + nodbConfig.USER + ':' + nodbConfig.PASSWORD + '@' + nodbConfig.HOST + '/' + nodbConfig.noDB + nodbConfig.end;
+const db_pathAZ = nodbConfig.dialectAZ + '://' + nodbConfig.USERAZ + ':' + nodbConfig.PASSWORDAZ + '@' + nodbConfig.HOSTAZ + '/' + nodbConfig.noDBAZ + nodbConfig.endAZ;
 
-mongoose.connect(db_path)
+mongoose.connect(db_pathAZ)
     .then((db) => {
         console.log('DB connnection successful!');
     })
