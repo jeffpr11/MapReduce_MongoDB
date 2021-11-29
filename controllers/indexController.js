7dafda2006e48492a0691cf9217913b761a9ba09
GeorgHenriquez <ghenriqu@espol.edu.ec>
2021-11-29 12:32:26 -0500

    map_reduce view

---
+++ controllers/indexController.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+
+module.exports = {
+    mapReduce: function (req, res) {
+        const limit =  10;
+        let page = 1;
+        if (req.query.page) page = req.query.page;
+        
+        fs.readFile('public/data/reduced.json', (err, data) => {
+            if (err) console.log(err);
+
+            let employment_wages = JSON.parse(data);
+            let keys = Object.keys(employment_wages);
+
+            let hasNext = keys[page+1] ? true : false;
+            let hasPrev = keys[page-1] ? true : false;
+
+            const infoPage = { actual: parseInt(page), hasPrev: hasPrev, 
+            hasNext: hasNext, pages: Math.floor(keys.length/limit), docs: keys.length };
+
+            let sliced_keys = keys.slice((page - 1) * limit + 1, page * limit + 1);
+
+            res.render('map_reduce', { title: 'Map Reduce', data: employment_wages, 
+            keys: sliced_keys, infoPage: infoPage, limit: limit });
+        });
+    }
+}
--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,28 @@
 var express = require('express');
 var router = express.Router();
+var indexController = require('../controllers/indexController.js');
+const fs = require('fs')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.redirect("/employment");
 });
 
+/*
+ * MAPREDUCE
+ */
+router.get('/map_reduce', indexController.mapReduce);
+
+
 router.get('/detail', function(req, res, next) {
   res.render('detail', { title: 'Detail' });
 });
+
 
 router.get('/new', function(req, res, next) {
   res.render('detail', { title: 'New' });
 });
 
-router.get('/map_reduce', function(req, res, next) {
-  res.render('map_reduce', { title: 'Map Reduce' });
-});
 
 
 module.exports = router;
